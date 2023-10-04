d7788242c4af49bf8a5168232aa00a4fdcf44ae4
German Renato Cobian <germancobian@hotmail.com>
2023-10-04 13:45:44 -0700

    Fix issues with re-rendering of website when clicking on back arrow

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const NavBar = () => (
   <div className="d-flex flex-row justify-content-between border-bottom">
-    <NavLink to="/" activeclassname="active-link "><h1 className="arrow mx-5">{'<'}</h1></NavLink>
+    <button type="button" className="btn btn-light" onClick={() => { window.location.href = '/'; }}>
+      <h2 className="mx-5">{'<'}</h2>
+    </button>
     <p className="col-6 mt-3">View Works In Our Collection</p>
     <span className="mic"><i className="fa fa-microphone mt-3" aria-hidden="true" /></span>
     <span><i className="fa fa-cog mx-5 mt-3" aria-hidden="true" /></span>
